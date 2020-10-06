a6c2e7900abbf2b943b20ce69aa30f19bee0eee2
Pontus Nydensten <pontusn@kth.se>
2020-10-06 13:32:10 +0200

    Some more styling

--- public/js/app/components/AppList.jsx
+++ public/js/app/components/AppList.jsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 
 import { observer } from 'mobx-react'
 import { useStore } from '../mobx'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 function Test({ result }) {
   return (
-    <div>
+    <div key={result.dataUrl}>
       <details>
         <summary class="white">{result.testUrl.replace('.html', '')}</summary>
         <iframe src={result.dataUrl}></iframe>
@@ -26,32 +27,40 @@ function AppList() {
 
   return (
     <div id="inspectResults">
-      {selectedApps.map(app => {
-        // const blockBlobClient = containerClient.getBlockBlobClient(blob.name)
-        // fetch(blockBlobClient.url).then(response => console.log(response.json()))
+      <TransitionGroup className="result-list">
+        {selectedApps.map(app => {
+          // const blockBlobClient = containerClient.getBlockBlobClient(blob.name)
+          // fetch(blockBlobClient.url).then(response => console.log(response.json()))
 
-        // const latestResults = app.results.find(r => r.commit === app.latestBuild)
+          // const latestResults = app.results.find(r => r.commit === app.latestBuild)
 
-        const latestTestrun = app.results.find(r => r.commit === app.latestBuild)
+          const latestTestrun = app.results.find(r => r.commit === app.latestBuild)
 
-        return (
-          <details>
-            <summary class="blue">
-              {app.name}
-              <div className="build">
-                {latestTestrun.date.toISOString().slice(0, 16).replace('T', ' ')} - #{app.latestBuild}
-              </div>
-            </summary>
-            <p>
-              {latestTestrun.tests
-                .filter(r => r.dataUrl.includes('.html'))
-                .map(result => {
-                  return <Test result={result}></Test>
-                })}
-            </p>
-          </details>
-        )
-      })}
+          return (
+            <CSSTransition
+              unmountOnExit
+              // in={inProp}
+              timeout={{ appear: 0, enter: 300, exit: 300 }}
+              classNames="list-transition"
+              appear
+            >
+              <details key={app.name}>
+                <summary class="blue">
+                  {app.name}
+                  <div className="build">
+                    {latestTestrun.date.toISOString().slice(0, 16).replace('T', ' ')} - #{app.latestBuild}
+                  </div>
+                </summary>
+                {latestTestrun.tests
+                  .filter(r => r.dataUrl.includes('.html'))
+                  .map(result => {
+                    return <Test result={result}></Test>
+                  })}
+              </details>
+            </CSSTransition>
+          )
+        })}
+      </TransitionGroup>
     </div>
   )
 }
